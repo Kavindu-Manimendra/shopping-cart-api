4033917a45230595aad7c439a68eeff537ca9e6c
Kavindu-Hasintha <110930930+Kavindu-Hasintha@users.noreply.github.com>
2024-10-19 20:06:38 +0530

    initial commit

---
+++ src/test/java/com/shoppingcart/scapi/ShoppingCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoppingcart.scapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
