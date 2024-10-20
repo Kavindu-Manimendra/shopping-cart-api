32d4966fb4b535a7f0bff85d254caf4201ee1e82
Kavindu-Hasintha <110930930+Kavindu-Hasintha@users.noreply.github.com>
2024-10-20 18:59:53 +0530

    ProductRepo added

--- src/main/java/com/shoppingcart/scapi/repo/ProductRepo.java
+++ src/main/java/com/shoppingcart/scapi/repo/ProductRepo.java
@@ -2,6 +2,8 @@ package com.shoppingcart.scapi.repo;
 
 import com.shoppingcart.scapi.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepo extends JpaRepository<Product, Long> {
 }
