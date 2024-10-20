12cdaa305f274ab74034d3f185a28f08e7c2c92b
Kavindu-Hasintha <110930930+Kavindu-Hasintha@users.noreply.github.com>
2024-10-20 19:00:32 +0530

    Get product by id func added

--- src/main/java/com/shoppingcart/scapi/service/ProductService.java
+++ src/main/java/com/shoppingcart/scapi/service/ProductService.java
@@ -1,12 +1,13 @@
 package com.shoppingcart.scapi.service;
 
 import com.shoppingcart.scapi.entity.Product;
+import com.shoppingcart.scapi.exception.ProductNotFoundException;
 
 import java.util.List;
 
 public interface ProductService {
     Product addProduct(Product product);
-    Product getProductById(Long id);
+    Product getProductById(Long id) throws ProductNotFoundException;
     void deleteProductById(Long id);
     void updateProduct(Product product, Long productId);
     List<Product> getAllProducts();
--- src/main/java/com/shoppingcart/scapi/service/impl/ProductServiceImpl.java
+++ src/main/java/com/shoppingcart/scapi/service/impl/ProductServiceImpl.java
@@ -1,4 +1,86 @@
 package com.shoppingcart.scapi.service.impl;
 
-public class ProductServiceImpl {
+import com.shoppingcart.scapi.dto.ResponseCode;
+import com.shoppingcart.scapi.entity.Product;
+import com.shoppingcart.scapi.exception.ProductNotFoundException;
+import com.shoppingcart.scapi.repo.ProductRepo;
+import com.shoppingcart.scapi.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ProductServiceImpl implements ProductService {
+    private ProductRepo productRepo;
+
+    @Override
+    public Product addProduct(Product product) {
+        return null;
+    }
+
+    @Override
+    public Product getProductById(Long id) throws ProductNotFoundException {
+        try {
+            Optional<Product> product = productRepo.findById(id);
+            if (product == null) {
+                ResponseCode.PRODUCT_NOT_FOUND.setReason("Invalid ID or Product ID does not exist in the database.");
+                throw new ProductNotFoundException(ResponseCode.PRODUCT_NOT_FOUND);
+            }
+
+            return product.get();
+        } catch (ProductNotFoundException e) {
+            throw new ProductNotFoundException(ResponseCode.PRODUCT_NOT_FOUND);
+        } catch (Exception e) {
+            ResponseCode.PRODUCT_NOT_FOUND.setReason(e.getMessage());
+            throw new ProductNotFoundException(ResponseCode.PRODUCT_NOT_FOUND);
+        }
+    }
+
+    @Override
+    public void deleteProductById(Long id) {
+
+    }
+
+    @Override
+    public void updateProduct(Product product, Long productId) {
+
+    }
+
+    @Override
+    public List<Product> getAllProducts() {
+        return List.of();
+    }
+
+    @Override
+    public List<Product> getProductsByCategory(String category) {
+        return List.of();
+    }
+
+    @Override
+    public List<Product> getProductsByBrand(String brand) {
+        return List.of();
+    }
+
+    @Override
+    public List<Product> getProductsByCategoryAndBrand(String category, String brand) {
+        return List.of();
+    }
+
+    @Override
+    public List<Product> getProductsByName(String name) {
+        return List.of();
+    }
+
+    @Override
+    public List<Product> getProductsByBrandAndName(String brand, String name) {
+        return List.of();
+    }
+
+    @Override
+    public Long countProductsByBrandAndName(String brand, String name) {
+        return 0;
+    }
 }
