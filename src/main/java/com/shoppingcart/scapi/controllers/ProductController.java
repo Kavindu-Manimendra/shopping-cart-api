6eca265368f823d651c5a8368fd6deed329252c9
Kavindu-Hasintha <110930930+Kavindu-Hasintha@users.noreply.github.com>
2024-11-14 12:40:28 +0530

    Get all products API added

--- src/main/java/com/shoppingcart/scapi/controllers/ProductController.java
+++ src/main/java/com/shoppingcart/scapi/controllers/ProductController.java
@@ -1,13 +1,36 @@
 package com.shoppingcart.scapi.controllers;
 
+import com.shoppingcart.scapi.dto.APIResponseDto;
+import com.shoppingcart.scapi.dto.ResponseCode;
+import com.shoppingcart.scapi.entity.Product;
+import com.shoppingcart.scapi.exception.ProductRetrivedFailedException;
 import com.shoppingcart.scapi.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("${api.prefix}/products")
 public class ProductController {
     private final ProductService productService;
+
+    @GetMapping("/all")
+    public ResponseEntity<APIResponseDto> getAllProducts() {
+        List<Product> products = null;
+        try {
+            products = productService.getAllProducts();
+        } catch (ProductRetrivedFailedException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(APIResponseDto.getInstance(e.getResponseCode()));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+        ResponseCode.SUCCESS.setReason("Product listing successful!");
+        return ResponseEntity.ok(APIResponseDto.getInstance(ResponseCode.SUCCESS, products));
+    }
 }
