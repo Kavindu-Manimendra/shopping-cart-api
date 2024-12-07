223f3b245d579e0bd723730508c71791b1b5dc1e
Kavindu-Hasintha <110930930+Kavindu-Hasintha@users.noreply.github.com>
2024-12-07 16:18:19 +0530

    removeItem func added

--- src/main/java/com/shoppingcart/scapi/entity/Cart.java
+++ src/main/java/com/shoppingcart/scapi/entity/Cart.java
@@ -29,4 +29,10 @@ public class Cart {
         item.setCart(this);
         // updateTotalAmount();
     }
+
+    public void removeItem(CartItem item) {
+        this.items.remove(item);
+        item.setCart(null);
+        // updateTotalAmount();
+    }
 }
