8ff1789dc3a69a00b834ecafe1c4252e6b216a0c
Kavindu-Hasintha <110930930+Kavindu-Hasintha@users.noreply.github.com>
2024-12-12 16:21:15 +0530

    CartItemService added

---
+++ src/main/java/com/shoppingcart/scapi/service/CartItemService.java
@@ -0,0 +1,7 @@
+package com.shoppingcart.scapi.service;
+
+public interface CartItemService {
+    void addItemToCart(Long cartId, Long productId, int quantity);
+    void removeItemFromCart(Long cartId, Long productId);
+    void updateItemQuantity(Long cartId, Long productId, int quantity);
+}
