8c2397d902a87a1e66956e8235e00b25d2d57f24
Kavindu-Hasintha <110930930+Kavindu-Hasintha@users.noreply.github.com>
2024-12-16 13:46:08 +0530

    getCart by Id API implemented

--- src/main/java/com/shoppingcart/scapi/controllers/CartController.java
+++ src/main/java/com/shoppingcart/scapi/controllers/CartController.java
@@ -1,7 +1,16 @@
 package com.shoppingcart.scapi.controllers;
 
+import com.shoppingcart.scapi.dto.APIResponseDto;
+import com.shoppingcart.scapi.dto.ResponseCode;
+import com.shoppingcart.scapi.entity.Cart;
+import com.shoppingcart.scapi.exception.CartNotFoundException;
+import com.shoppingcart.scapi.exception.CartSaveFailedException;
 import com.shoppingcart.scapi.service.CartService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +20,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class CartController {
     private final CartService cartService;
 
-
+    @GetMapping("/{cartId}/my-cart")
+    public ResponseEntity<APIResponseDto> getCart(@PathVariable Long cartId) {
+        Cart cart = null;
+        try {
+            cart = cartService.getCart(cartId);
+        } catch (CartNotFoundException | CartSaveFailedException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(APIResponseDto.getInstance(e.getResponseCode()));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+        ResponseCode.SUCCESS.setReason("Get cart successful!");
+        return ResponseEntity.ok(APIResponseDto.getInstance(ResponseCode.SUCCESS, cart));
+    }
 }
