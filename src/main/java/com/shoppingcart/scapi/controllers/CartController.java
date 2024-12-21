42374c8ea9de69c3a828ee6255a830f64a8ce42f
Kavindu-Hasintha <110930930+Kavindu-Hasintha@users.noreply.github.com>
2024-12-21 14:12:43 +0530

    clearCart by cart id API implemented

--- src/main/java/com/shoppingcart/scapi/controllers/CartController.java
+++ src/main/java/com/shoppingcart/scapi/controllers/CartController.java
@@ -3,16 +3,14 @@ package com.shoppingcart.scapi.controllers;
 import com.shoppingcart.scapi.dto.APIResponseDto;
 import com.shoppingcart.scapi.dto.ResponseCode;
 import com.shoppingcart.scapi.entity.Cart;
+import com.shoppingcart.scapi.exception.CartClearFailedException;
 import com.shoppingcart.scapi.exception.CartNotFoundException;
 import com.shoppingcart.scapi.exception.CartSaveFailedException;
 import com.shoppingcart.scapi.service.CartService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -33,4 +31,19 @@ public class CartController {
         ResponseCode.SUCCESS.setReason("Get cart successful!");
         return ResponseEntity.ok(APIResponseDto.getInstance(ResponseCode.SUCCESS, cart));
     }
+
+    @DeleteMapping("/{cartId}/clear")
+    public ResponseEntity<APIResponseDto> clearCart(@PathVariable Long cartId) {
+        try {
+            cartService.clearCart(cartId);
+        } catch (CartClearFailedException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(APIResponseDto.getInstance(e.getResponseCode()));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+        ResponseCode.SUCCESS.setReason("Clear cart successful!");
+        return ResponseEntity.ok(APIResponseDto.getInstance(ResponseCode.SUCCESS));
+    }
+
+
 }
