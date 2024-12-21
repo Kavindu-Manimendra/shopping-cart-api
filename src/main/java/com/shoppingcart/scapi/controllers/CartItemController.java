96a98ecd3454568809ab96a971d68b4aab4e1170
Kavindu-Hasintha <110930930+Kavindu-Hasintha@users.noreply.github.com>
2024-12-21 18:01:38 +0530

    removeItemFromCart API is implemented

--- src/main/java/com/shoppingcart/scapi/controllers/CartItemController.java
+++ src/main/java/com/shoppingcart/scapi/controllers/CartItemController.java
@@ -2,15 +2,14 @@ package com.shoppingcart.scapi.controllers;
 
 import com.shoppingcart.scapi.dto.APIResponseDto;
 import com.shoppingcart.scapi.dto.ResponseCode;
+import com.shoppingcart.scapi.exception.CartItemNotFoundException;
+import com.shoppingcart.scapi.exception.CartItemRemoveFailedException;
 import com.shoppingcart.scapi.exception.CartItemSaveFailedException;
 import com.shoppingcart.scapi.service.CartItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -29,6 +28,19 @@ public class CartItemController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
         ResponseCode.SUCCESS.setReason("Cart item added to cart successfully!");
+        return ResponseEntity.ok(APIResponseDto.getInstance(ResponseCode.SUCCESS));
+    }
+
+    @DeleteMapping("/cart/{cartId}/item/{itemId}/remove")
+    public ResponseEntity<APIResponseDto> removeItemFromCart(@PathVariable Long cartId, @PathVariable Long itemId) {
+        try {
+            cartItemService.removeItemFromCart(cartId, itemId);
+        } catch (CartItemNotFoundException | CartItemRemoveFailedException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(APIResponseDto.getInstance(e.getResponseCode()));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+        ResponseCode.SUCCESS.setReason("Cart item removed from cart successfully!");
         return ResponseEntity.ok(APIResponseDto.getInstance(ResponseCode.SUCCESS));
     }
 }
