1af98f7dea7779cedc07d31b4e4082f78d57a051
Kavindu-Hasintha <110930930+Kavindu-Hasintha@users.noreply.github.com>
2024-12-25 14:14:48 +0530

    Order entity added

---
+++ src/main/java/com/shoppingcart/scapi/entity/Order.java
@@ -0,0 +1,29 @@
+package com.shoppingcart.scapi.entity;
+
+import com.shoppingcart.scapi.enums.OrderStatus;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderId;
+    private LocalDate orderedDate;
+    private BigDecimal totalAmount;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<OrderItem> orderItems;
+}
