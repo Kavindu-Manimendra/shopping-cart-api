fdf11621d774b298dd80e62c7c0258f7031695b9
Kavindu-Hasintha <110930930+Kavindu-Hasintha@users.noreply.github.com>
2024-12-26 20:37:02 +0530

    OrderItem entity added

---
+++ src/main/java/com/shoppingcart/scapi/entity/OrderItem.java
@@ -0,0 +1,35 @@
+package com.shoppingcart.scapi.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private int quantity;
+    private BigDecimal price;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    public OrderItem(Product product, Order order, BigDecimal price, int quantity) {
+        this.product = product;
+        this.order = order;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
