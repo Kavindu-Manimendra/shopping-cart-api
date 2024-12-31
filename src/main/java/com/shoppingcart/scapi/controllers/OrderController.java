388e3800b1f9ecf89702daaab4adbae2905550a1
Kavindu-Hasintha <110930930+Kavindu-Hasintha@users.noreply.github.com>
2024-12-31 16:02:37 +0530

    getOrder By Id API implemented

--- src/main/java/com/shoppingcart/scapi/controllers/OrderController.java
+++ src/main/java/com/shoppingcart/scapi/controllers/OrderController.java
@@ -3,15 +3,13 @@ package com.shoppingcart.scapi.controllers;
 import com.shoppingcart.scapi.dto.APIResponseDto;
 import com.shoppingcart.scapi.dto.ResponseCode;
 import com.shoppingcart.scapi.entity.Order;
+import com.shoppingcart.scapi.exception.OrderNotFoundException;
 import com.shoppingcart.scapi.exception.PlaceOrderFailedException;
 import com.shoppingcart.scapi.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -20,7 +18,7 @@ public class OrderController {
     private final OrderService orderService;
 
     @PostMapping("/order/{userId}")
-    public ResponseEntity<APIResponseDto> createOrder(@PathVariable Long userId) {
+    public ResponseEntity<APIResponseDto> createOrder(@RequestParam Long userId) {
         Order order = null;
         try {
             order = orderService.placeOrder(userId);
@@ -32,4 +30,20 @@ public class OrderController {
         ResponseCode.SUCCESS.setReason("Order created successfully!");
         return ResponseEntity.ok(APIResponseDto.getInstance(ResponseCode.SUCCESS, order));
     }
+
+    @GetMapping("/{orderId}/order")
+    public ResponseEntity<APIResponseDto> getOrderById(@PathVariable Long orderId) {
+        Order order = null;
+        try {
+            order = orderService.getOrder(orderId);
+        } catch (OrderNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(APIResponseDto.getInstance(e.getResponseCode()));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+        ResponseCode.SUCCESS.setReason("Order retrieved successfully!");
+        return ResponseEntity.ok(APIResponseDto.getInstance(ResponseCode.SUCCESS, order));
+    }
+
+
 }
