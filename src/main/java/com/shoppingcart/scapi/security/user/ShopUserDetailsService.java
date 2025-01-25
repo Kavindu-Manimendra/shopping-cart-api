ff3317a0440d69da21640c7212cdd0dd20ad1fc2
Kavindu-Hasintha <110930930+Kavindu-Hasintha@users.noreply.github.com>
2025-01-25 19:17:42 +0530

    ShopUserDetailsService implemented

--- src/main/java/com/shoppingcart/scapi/security/user/ShopUserDetailsService.java
+++ src/main/java/com/shoppingcart/scapi/security/user/ShopUserDetailsService.java
@@ -1,13 +1,24 @@
 package com.shoppingcart.scapi.security.user;
 
+import com.shoppingcart.scapi.entity.User;
+import com.shoppingcart.scapi.repo.UserRepo;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class ShopUserDetailsService implements UserDetailsService {
+    private final UserRepo userRepo;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepo.findByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found!");
+        }
+        return ShopUserDetails.buildUserDetails(user);
     }
 }
