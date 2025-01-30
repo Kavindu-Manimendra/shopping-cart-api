33723434405b3478fe26d0583b782ba9d60b5c3e
Kavindu-Hasintha <110930930+Kavindu-Hasintha@users.noreply.github.com>
2025-01-30 14:54:54 +0530

    getUsernameFromToken and validateToken func added

--- src/main/java/com/shoppingcart/scapi/security/jwt/JwtUtils.java
+++ src/main/java/com/shoppingcart/scapi/security/jwt/JwtUtils.java
@@ -1,19 +1,24 @@
 package com.shoppingcart.scapi.security.jwt;
 
 import com.shoppingcart.scapi.security.user.ShopUserDetails;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Component;
 
 import java.security.Key;
 import java.util.Date;
 import java.util.List;
 
+@Component
 public class JwtUtils {
+    @Value("${auth.token.jwtSecret}")
     private String jwtSecret;
+
+    @Value("${auth.token.expirationInMils}")
     private int expirationTime;
 
     public String generateTokenForUser(Authentication authentication) {
@@ -34,5 +39,26 @@ public class JwtUtils {
 
     private Key key() {
         return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+    }
+
+    public String getUsernameFromToken(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key())
+                .build()
+                .parseClaimsJws(token)
+                .getBody().getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(key())
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (ExpiredJwtException | UnsupportedJwtException | MalformedJwtException | SignatureException
+                 | IllegalArgumentException e) {
+            throw new JwtException(e.getMessage());
+        }
     }
 }
