bc14b13093b17861a1d4edbd15d20c09fc10b8b9
Kavindu-Hasintha <110930930+Kavindu-Hasintha@users.noreply.github.com>
2025-10-04 12:42:56 +0530

    User services added

---
+++ src/main/java/com/shoppingcart/scapi/security/jwt/impl/UserServiceImpl.java
@@ -0,0 +1,27 @@
+package com.shoppingcart.scapi.security.jwt.impl;
+
+import com.shoppingcart.scapi.repo.UserRepo;
+import com.shoppingcart.scapi.security.jwt.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserRepo userRepo;
+
+    @Override
+    public UserDetailsService userDetailsService() {
+        return new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                return userRepo.findByEmail(username)
+                        .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+            }
+        };
+    }
+}
